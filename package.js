c8c42349656e3a86cabd8d95bf381c5ccfc50b61
sam@hatoum.net <sam@hatoum.net>
2014-11-17 20:28:47 -0800

    initial commit

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'velocity:test-proxy',
+  summary: 'Dynamically created package to expose test files to mirrors',
+  version: '0.0.1',
+  debugOnly: true
+});
+
+Npm.depends({
+});
+
+Package.on_use(function (api) {
+});
